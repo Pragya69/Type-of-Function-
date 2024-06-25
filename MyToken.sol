215e93a8109a37b445786d232db107fa2ad177bf
Pragya69 <165126359+Pragya69@users.noreply.github.com>
2024-06-26 00:26:55 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor() ERC20("MyToken", "MTK") Ownable(msg.sender) {}
+    constructor() ERC20("MyToken", "PTH") Ownable(msg.sender) {}
 
     // Only the owner can mint new tokens to a provided address
     function mintTokens(address recipient, uint256 tokenAmount) external onlyOwner {
